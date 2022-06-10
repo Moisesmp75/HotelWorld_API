96d16841d0f1e04202093051bf4c14639ee31611
Miner <54141767+Apolosmllb@users.noreply.github.com>
2022-06-09 21:55:12 -0500

    Update ProductService.java

--- src/main/java/com/example/hellohotel/HelloHotel/domain/service/ProductService.java
+++ src/main/java/com/example/hellohotel/HelloHotel/domain/service/ProductService.java
@@ -1,1 +1,17 @@
+package com.example.hellohotel.HelloHotel.domain.service;
 
+import com.example.hellohotel.HelloHotel.domain.model.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+
+import java.util.List;
+
+public interface ProductService {
+    List<Product> getAll();
+    Page<Product> getAll(Pageable pageable);
+    Product getById(Long studentId);
+    Product create(Product product);
+    Product update(Long studentId, Product request);
+    ResponseEntity<?> delete(Long product_Id);
+}
